b57ec0a02aae3a525b47d581295ebbd6ccce4b24
Aditi-2840 <kaditi2840@gmail.com>
2020-06-24 21:22:15 +0530

    added all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'scroll';
+export class AppComponent implements OnInit {
+  constructor(private http:HttpClient, private spinner:NgxSpinnerService){
+
+  }
+  allpost;
+  notEmptypost=true;
+  notScrolly=true;
+  ngOnInit(){
+    this.loadInitPost();
+  }
+  //load initial 6 posts
+  loadInitPost() {
+    const url = 'http://tlino.96.lt/api/getblogpost';
+    this.http.get(url).subscribe(data => {
+      this.allpost = data[0];
+    });
+  }
+  onScroll()
+  {
+    if(this.notScrolly && this.notEmptypost){
+      this.spinner.show();
+      this.notScrolly=false;
+      this.loadNextPost();
+    }
+    
+  }
+  // load th next 6 posts
+ loadNextPost() {
+  const url = 'http://tlino.96.lt/api/getnextpost';
+  // return last post from the array
+  const lastPost = this.allpost[this.allpost.length - 1];
+  // get id of last post
+  //  backend of this app use this id to get next 6 post
+  const lastPostId = lastPost.id;
+  // sent this id as key value pare using formdata()
+  const dataToSend = new FormData();
+  dataToSend.append('id', lastPostId);
+  // call http request
+  this.http.post(url, dataToSend)
+  .subscribe( (data: any) => {
+
+     const newPost = data[0];
+    this.loadNextPost();
+     
+     if (newPost.length === 0 ) {
+       this.notEmptypost =  true;
+       this.loadNextPost();
+       
+     }
+     // add newly fetched posts to the existing post
+     this.allpost = this.allpost.concat(newPost);
+
+     this.notScrolly = false;
+   });
+}
+
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {InfiniteScrollModule} from "ngx-infinite-scroll";
+import { NgxSpinnerModule} from "ngx-spinner";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InfiniteScrollModule,
+    NgxSpinnerModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
